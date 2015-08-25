c2c111d85ef14efcfe1eb76a5f97ffabce68d518
Luiz Carlos <luizgadao@gmail.com>
2015-08-25 19:40:10 -0300

    floating action button and colors

--- app/src/main/java/com/luizgadao/testzup/MainActivityFragment.java
+++ app/src/main/java/com/luizgadao/testzup/MainActivityFragment.java
@@ -1,7 +1,9 @@
 package com.luizgadao.testzup;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,8 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private static final String TAG = MainActivityFragment.class.getSimpleName();
+
     public MainActivityFragment() {
     }
 
@@ -18,5 +22,18 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView( LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState ) {
         return inflater.inflate( R.layout.fragment_main, container, false );
+    }
+
+    @Override
+    public void onViewCreated( View view, Bundle savedInstanceState ) {
+        super.onViewCreated( view, savedInstanceState );
+
+        FloatingActionButton buttonAddMovie = ( FloatingActionButton ) view.findViewById( R.id.ab_add_movie );
+        buttonAddMovie.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                Log.i(TAG, "click floating action button");
+            }
+        } );
     }
 }
